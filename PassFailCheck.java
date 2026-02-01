f92d86e50560d1c4eb80e81884c372b8f16f1f35
majaliwa-joel <majaliwajoel64@gmail.com>
2026-02-01 13:20:16 +0300

    Add files via upload

---
+++ PassFailCheck.java
@@ -0,0 +1,13 @@
+public class PassFailCheck {
+    public static void main(String[] args) {
+
+        int score = 50;
+
+        if (score >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+    }
+}
